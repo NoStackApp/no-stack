986b831d581a6534819ab05d7fe7bed6ee009ceb
Rem Lampa <rem@remlampa.com>
2019-09-13 13:55:08 +0800

    chore: fix jest configs :wrench:

--- jest.config.js
+++ jest.config.js
@@ -5,13 +5,8 @@ module.exports = {
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
   },
   testPathIgnorePatterns: [`node_modules`, `dist`],
-  setupFilesAfterEnv: [
-    '@testing-library/react/cleanup-after-each',
-    '<rootDir>/jest.setup.js',
-  ],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(spec))\\.tsx?$',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  preset: 'ts-jest',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
 };
