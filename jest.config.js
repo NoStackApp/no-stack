75a98736475b9bc5140357cd323cd64e3776238d
Rem Lampa <rem@remlampa.com>
2019-09-13 14:31:55 +0800

    chore(react): upgrade react :arrow_up:

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,6 @@ module.exports = {
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
   },
   testPathIgnorePatterns: [`node_modules`, `dist`],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   preset: 'ts-jest',
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
