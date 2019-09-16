29a333c2ed222bfdccaf5532b269572add6c4ede
Rem Lampa <rem@remlampa.com>
2019-09-16 23:30:53 +0800

    chore(jest): fix jest & Typescript interop :wrench:

--- .babelrc.js
+++ .babelrc.js
@@ -1,13 +1,15 @@
 module.exports = {
   presets: [
-    "@babel/preset-typescript",
     [
       "@babel/preset-env",
       {
-        modules: false
-      }
+        targets: {
+          node: 'current',
+        },
+      },
     ],
-    "@babel/preset-react"
+    "@babel/preset-react",
+    "@babel/preset-typescript"
   ],
   plugins: [
     "@babel/plugin-proposal-class-properties",
